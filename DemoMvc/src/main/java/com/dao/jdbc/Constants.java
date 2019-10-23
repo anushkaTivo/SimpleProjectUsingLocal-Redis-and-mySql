5c28e81303cdaebbb8804e857453365fc1ad8acd
anushka <anu32512@gmail.com>
2019-10-23 15:26:56 +0530

    My SampleWebProject

---
+++ DemoMvc/src/main/java/com/dao/jdbc/Constants.java
@@ -0,0 +1,16 @@
+package com.dao.jdbc;
+
+public class Constants {
+
+	//FOR MAIN DATABASE
+	public static final String driverName = "com.mysql.cj.jdbc.Driver";
+	public static final String hostDb = "jdbc:mysql://localhost:3306/sample?autoReconnect=true&useSSL=false";
+	public static final String userName = "root";
+	public static final String password = "";
+	
+	//FOR CACHE
+	public static final String hostName = "localhost";
+	public static final int port = 6379; 
+	
+	
+}
